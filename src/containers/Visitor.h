1781b26939d22c8f6432caa8450166aacc746c93
marcel <marcel.hajek7@gmail.com>
2026-08-17 08:30:18 +0200

    ...

--- src/containers/Visitor.h
+++ src/containers/Visitor.h
@@ -10,34 +10,11 @@ namespace ct {
 template <typename ObjectType>
 class Visitor final {
    public:
-    Visitor() : objectHeads(), bump(), objectBytes() {}
-
-    template <typename PushedObjectType, typename... Args>
-        requires std::constructible_from<PushedObjectType, Args...>
-    void create(Args... args) {
-        constexpr std::size_t objectSize = sizeof(PushedObjectType);
-        new (objectBytes)
-            PushedObjectType(std::forward<Args>(args)...);
-
-        objectHeads.emplace_back(objectSize);
-    }
-
+    Visitor() = default;
     Visitor(Visitor&&) = delete;
     Visitor& operator=(Visitor&&) = delete;
     Visitor(const Visitor&) = delete;
     Visitor& operator=(const Visitor&) = delete;
-
-    template <typename IterFunc>
-    void forEach(IterFunc iterFunc) {
-        std::byte* byteIter = objectBytes;
-
-        for (auto& objectHead : objectHeads) {
-            iterFunc(reinterpret_cast<ObjectType*>(byteIter));
-
-            byteIter += objectHead;
-        }
-    }
-
     ~Visitor() {
         std::cout << "dtor not implemented yet";
         /*
@@ -46,10 +23,31 @@ class Visitor final {
         */
     }
 
+    template <typename PushedObjectType, typename... Args>
+        requires std::constructible_from<PushedObjectType, Args...>
+    void create(Args... args) {
+        constexpr std::size_t objectSize = sizeof(PushedObjectType);
+        std::byte* newObjectBytes = nullptr;
+        new (newObjectBytes)
+            PushedObjectType(std::forward<Args>(args)...);
+
+        objectHeads.emplace_back(objectSize);
+    }
+
+    template <typename IterFunc>
+    void forEach(IterFunc iterFunc) {
+        auto byteIter = objectBytes.begin().base();
+
+        for (auto& objectHead : objectHeads) {
+            iterFunc(reinterpret_cast<ObjectType*>(byteIter));
+
+            byteIter += objectHead;
+        }
+    }
+
    private:
+    std::vector<std::byte> objectBytes;
     std::vector<std::size_t> objectHeads;
-    std::size_t bump = 0;
-    std::byte* objectBytes;
 };
 }  // namespace ct
 #endif  // CORETYPES_VISITOR_H
